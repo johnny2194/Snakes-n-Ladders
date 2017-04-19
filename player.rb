bdf12b0f0cb8d8eee8ba5fbd099585548f7ad2f0
Johnny Watson <johnny@streetchange.org.uk>
2017-04-19 11:30:45 +0100

    add roll_dice to player

--- player.rb
+++ player.rb
@@ -9,4 +9,13 @@ class Player
   def move(new_position)
     @position = new_position
   end 
+
+  def increment_position(number_incremented)
+    @position += number_incremented
+  end
+
+  def roll_dice
+    dice = Random.new
+    return dice.rand(1..6)
+  end 
 end 
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -19,7 +19,18 @@ class TestPlayer < MiniTest::Test
     @player.move(3)
     assert_equal(3, @player.position)
   end 
-     
+    
+  def test_player_can_increment_position
+    @player.increment_position(3)
+    assert_equal(4, @player.position)
+  end
+
+  def test_player_rolls_dice_returns_number_between_one_and_six
+    number_rolled = @player.roll_dice
+    is_between_one_and_six = number_rolled >= 1 && number_rolled <= 6
+    assert(is_between_one_and_six)
+  end 
+  
 end
 
 
