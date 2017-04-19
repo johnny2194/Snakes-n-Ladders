2cfdfb0b8aa6b1fa420aabaa70805c58499dd5d5
Johnny Watson <johnny@streetchange.org.uk>
2017-04-19 11:11:12 +0100

    Add move function to player

--- player.rb
+++ player.rb
@@ -1,7 +1,12 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :position
   def initialize(name)
     @name = name
+    @position = 1
+  end 
+
+  def move(new_position)
+    @position = new_position
   end 
 end 
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -10,6 +10,16 @@ class TestPlayer < MiniTest::Test
   def test_get_player_name
     assert_equal("Bob", @player.name)
   end
+
+  def test_get_player_position
+    assert_equal(1, @player.position)
+  end
+
+  def test_player_can_move
+    @player.move(3)
+    assert_equal(3, @player.position)
+  end 
+     
 end
 
 
