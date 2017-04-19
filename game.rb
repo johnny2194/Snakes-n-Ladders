68a15ebc81b18b656a1efb19ec4a2dd009e405d1
Johnny Watson <johnny@streetchange.org.uk>
2017-04-19 12:13:16 +0100

    add method for adding player to game

--- game.rb
+++ game.rb
@@ -1,6 +1,13 @@
+require_relative('player.rb')
+
 class Game
   attr_reader :players_array
   def initialize()
     @players_array = []
   end 
+
+  def add_player(name)
+    @players_array << Player.new(name)
+  end 
+
 end 
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -1,7 +1,7 @@
 require ('minitest/autorun')
 require ('minitest/rg')
 require_relative ("../game.rb")
-
+require_relative('../player.rb')
 
 class TestGame < MiniTest::Test
   def setup
@@ -11,4 +11,12 @@ class TestGame < MiniTest::Test
   def test_has_empty_players_array
     assert_equal(0, @game.players_array.count)
   end
+
+  def test_can_add_new_player
+   @game.add_player("Bob")
+   actual = @game.players_array.first.name
+   assert_equal("Bob", actual)
+  end 
+
+  # this adds a player to players array then checks if that player is there by making sure it has the same name as given when added
 end
